1af0e4254f611b93fcf842dad58c31bc622a2846
hugcis <hmj.cisneros@gmail.com>
2019-06-12 15:46:29 +0200

    Group plotting scripts into a single file

--- generate_frames.sh
+++ generate_frames.sh
@@ -1,3 +1,14 @@
-./2d_automaton $1
-python plot_2d.py $1 2
-open rule_gif/tmp*.png
+# Usage: `./generate_frames.sh [rule] [n_states (default 3)]`
+#
+# This script will run a simulation of a cellular automaton specified by the
+# rule hash and number of states and generate a gif file of its evolution.
+# TODO: add support for options in both the bin/automaton executable and the
+#       python script.
+
+if [ -z $2 ];
+   then STATES=3;
+else STATES=$2;
+fi
+./bin/automaton 2d -n $STATES -f "data_2d_$STATES/map/$1.map" || { echo 'Map file not found'; exit 1; }
+python plot_2d.py $1 $STATES
+open rule_gif/tmp.gif
