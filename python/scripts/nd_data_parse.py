25f77329c190277d88b27110301039733be24c3d
hugcis <hmj.cisneros@gmail.com>
2019-08-08 16:15:58 +0200

    Update results script.

--- python/scripts/nd_data_parse.py
+++ python/scripts/nd_data_parse.py
@@ -1,3 +1,6 @@
+"""
+Read 2 states data.
+"""
 import os
 import pickle as pkl
 import matplotlib.pyplot as plt
@@ -5,14 +8,18 @@ import numpy as np
 
 ST = 2
 
-results_dic = {}
-cpl = []
-nn_names = []
+RESULTS_DIC = {}
+CPL = []
+NN_NAMES = []
 
-def get_results(results_dic, neigh, ts, ratio=False, exp=None, head=None):
+def get_results(results_dic, neigh, t_step,
+                ratio=False, exp=None, head=None):
+    """ Parse data from result dic. """
+
     static = []
     if ratio:
-        fun = lambda x, b: (x[1]/(x[0] if x[0] > 0 else x[0] + 1e-12)) if not b else 1
+        fun = lambda x, b: (x[1]/(x[0] if x[0] > 0
+                                  else x[0] + 1e-12)) if not b else 1
     else:
         fun = lambda x, b: x[1] - x[0] if not b else 0
 
@@ -29,11 +36,12 @@ def get_results(results_dic, neigh, ts, ratio=False, exp=None, head=None):
 
     if head is not None:
         return ([rule for rule in head if rule in results_dic],
-                np.array([fun(results_dic[rule][neigh][ts], rule in static)
+                np.array([fun(results_dic[rule][neigh][t_step],
+                              rule in static)
                           for rule in head if rule in results_dic]))
 
     return ([rule for rule in results_dic],
-            np.array([fun(results_dic[rule][neigh][ts], rule in static)
+            np.array([fun(results_dic[rule][neigh][t_step], rule in static)
                       for rule in results_dic]))
 
 for i in [t for t in os.listdir('data_2d_{}/nn/'.format(ST))
@@ -47,21 +55,24 @@ for i in [t for t in os.listdir('data_2d_{}/nn/'.format(ST))
 
             s = f.read().split('\n')
             try:
-                cpl.append(int(s[-2].split('    ')[1]))
-            except Exception:
+                CPL.append(int(s[-2].split('    ')[1]))
+            except IndexError:
                 continue
+            except ValueError:
+                continue
+
         name = i.split('.')[0].strip('nn')
-        nn_names.append(name)
+        NN_NAMES.append(name)
         v = list(map(lambda x: [float(i) for i in x.split('    ')], x.split('\n')))
 
-        results_dic[name] = {}
+        RESULTS_DIC[name] = {}
 
         for radius in range(1, 8):
-            results_dic[name][radius] = {
+            RESULTS_DIC[name][radius] = {
                 300: [v[radius - 1][0], v[radius - 1][1]]
             }
 
-pkl.dump(results_dic, open('python/data/2d_dataset.pkl', 'wb+'))
-print(" ".join(nn_names))
-plt.plot(get_results(results_dic, 3, 300, exp="sum")[1])
+pkl.dump(RESULTS_DIC, open('python/data/2d_dataset.pkl', 'wb+'))
+print(" ".join(NN_NAMES))
+plt.plot(get_results(RESULTS_DIC, 3, 300, exp="sum")[1])
 plt.show()
